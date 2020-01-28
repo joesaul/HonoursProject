d1f7891116d14117750769882e0e2b4cf83b7f86
joesaul <60140318+joesaul@users.noreply.github.com>
2020-01-28 17:01:31 +0000

    editing UI

--- app/src/main/java/com/example/honoursproject_opendayapp/MainActivity.java
+++ app/src/main/java/com/example/honoursproject_opendayapp/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private Button campusmap_button;
     
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -85,6 +88,13 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, CampusMap.class);
         startActivity(intent);
     }
+
+    //@Override
+    //public boolean onCreateOptionsMenu(Menu menu){
+    //    MenuInflater inflater = getMenuInflater();
+    //    inflater.inflate(R.menu.example_menu, menu);
+    //    return true;
+    //}
 
 
 }
